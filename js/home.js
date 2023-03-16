4679a661b98ad33a09640b3ba250195bf631cade
unknown <dhruv@leo9studio.com>
2023-03-16 08:54:20 +0530

    addition

---
+++ js/home.js
@@ -0,0 +1,13 @@
+
+window.addEventListener("load", (event) => {
+
+    const swiperWork = new Swiper('.swiper-work', {
+        slidesPerView: 4,
+        spaceBetween: 55,
+        loop: true,
+        speed: 5000,
+        autoplay: {
+            delay: 0
+        }
+    });
+});
