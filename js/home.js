03458cc3ca2479db89b9ca7d8f3a96571f9f8bf2
unknown <dhruv@leo9studio.com>
2023-03-18 12:46:30 +0530

    changes

--- js/home.js
+++ js/home.js
@@ -10,4 +10,14 @@ window.addEventListener("load", (event) => {
             delay: 0
         }
     });
+
+    const swiperClients = new Swiper('.clients-logo-swiper', {
+        slidesPerView: 1,
+        spaceBetween: 55,
+        speed: 800,
+        navigation: {
+            nextEl: '.swiper-button-next-logo',
+            prevEl: '.swiper-button-prev-logo',
+        },
+    });
 });
