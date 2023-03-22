2440e58934dc9bf5f69bf1edbdbe402e3b2dcaf4
piyush panchal <piyushpanchal004@gmail.com>
2023-03-22 20:59:32 +0530

    footer added

--- happy-wp/wp-content/themes/happy/footer.php
+++ happy-wp/wp-content/themes/happy/footer.php
@@ -1,3 +1,18 @@
+
+<?php
+                                
+                                $queryObject = new  Wp_Query(array(
+                                    'posts_per_page'   => '1',
+                                    'post_type'        => 'footer',
+                                    'post_status'      => 'publish'
+                                )); ?>
+                        <?php
+                                $cnt = 0;
+
+                            if ($queryObject->have_posts()) {
+                                while ($queryObject->have_posts()) : $queryObject->the_post();
+                                    $title = get_the_title();
+                            ?>
 <footer id="footer" class="footer">    
     <div class="footer-wrap">
         <div class="footer-box">
@@ -6,25 +21,25 @@
                     <div class="w50 w-768-100">
                         <div class="contact-links">
                             <div class="link">
-                                <a href="tel:919-397-6667"><img src="<?php bloginfo('template_url'); ?>/assets/img/call.svg" alt=""> 919-397-6667</a>
+                                <a href="tel:<?php echo get_field('number')?>"><img src="<?php bloginfo('template_url'); ?>/assets/img/call.svg" alt=""> <?php echo get_field('number')?></a>
                             </div>
                             <div class="link">
-                                <a href="javascript:void(0)"><img src="<?php bloginfo('template_url'); ?>/assets/img/fax.svg" alt="">Morrisville, North Carolina 27560</a>
+                                <a href="javascript:void(0)"><img src="<?php bloginfo('template_url'); ?>/assets/img/fax.svg" alt=""><?php echo get_field('address')?></a>
                             </div>
                             <div class="link">
                                 <a
-                                    href="mailto:ryelle@happyliferecruitingagency.com"><img src="<?php bloginfo('template_url'); ?>/assets/img/mail.svg" alt="">ryelle@happyliferecruitingagency.com</a>
+                                    href="mailto:<?php echo get_field('email')?>"><img src="<?php bloginfo('template_url'); ?>/assets/img/mail.svg" alt=""><?php echo get_field('email')?></a>
                             </div>
                             <div class="follow-wrap">
                                 <p>Follow us</p>
                                 <div class="follow-links">
-                                    <a href="">
+                                    <a href="<?php echo get_field('facebook')?>">
                                         <img src="<?php bloginfo('template_url'); ?>/assets/img/facebook.png" alt="">
                                     </a>
-                                    <a href="">
+                                    <a href="<?php echo get_field('instagram')?>">
                                         <img src="<?php bloginfo('template_url'); ?>/assets/img/insta.png" alt="">
                                     </a>
-                                    <a href="">
+                                    <a href="<?php echo get_field('linkedin')?>">
                                         <img src="<?php bloginfo('template_url'); ?>/assets/img/linkedin.png" alt="">
                                     </a>
                                 </div>
@@ -36,10 +51,10 @@
                             <div class="logo">
                                 <img src="<?php bloginfo('template_url'); ?>/assets/img/logo.svg" alt="">
                             </div>
-                            <p>Happy Life Recruiting Agency.com. All rights reserved.</p>
+                            <p><?php echo get_field('copy_rights')?></p>
                             <div class="privacy-wrap">
-                                <a href="">Privacy Policy</a>
-                                <a href="">Terms & conditions</a>
+                                <a href="<?php echo get_field('privacy_policy')?>">Privacy Policy</a>
+                                <a href="<?php echo get_field('terms')?>">Terms & conditions</a>
                             </div>
                         </div>
                     </div>
@@ -48,7 +63,9 @@
         </div>
     </div>
 </footer>
-
+<?php endwhile;
+                                } wp_reset_query();  // Restore global post data stomped by the_post().
+                                ?>
 <?php wp_footer(); ?>
 </body>
 
